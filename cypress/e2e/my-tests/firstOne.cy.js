e7ec727438f63bd3b942c36d15e15c168850b3d2
Andrew Ilin <eternal.wanderer.z@gmail.com>
2026-03-02 12:52:18 +0300

    feat: testing features for blog and url-shortener

--- cypress/e2e/my-tests/firstOne.cy.js
+++ cypress/e2e/my-tests/firstOne.cy.js
@@ -2,14 +2,40 @@ describe("Navigation", () => {
   it("should navigate to the home page", () => {
     // Start from the index page
     cy.visit("http://localhost:3000/");
+  });
+  it("should navigate to the blog page", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('a[href="/blog"]').first().click();
+    cy.url().should("include", "/blog");
 
-    // Find a link with an href attribute containing "about" and click it
-    cy.get('a[href*="about"]').click();
+    cy.get("h1").contains("Blog");
+  });
 
-    // The new url should include "/about"
-    cy.url().should("include", "/about");
+  // it("should navigate to the url shortener page", () => {
+  //   cy.visit("http://localhost:3000/");
+  //   cy.get('a[href="/url-shortener"]').first().click();
+  //   cy.url().should("include", "/url-shortener");
 
-    // The new page should contain an h1 with "About"
-    cy.get("h1").contains("About");
+  //   cy.get("h1").contains("Url Shortener");
+  // });
+  it("should navigate to test link and get redirrected", () => {
+    cy.visit("http://localhost:3000/go/gogogo");
+    cy.url().should("include", "google.com");
   });
 });
+
+// describe("Url Shortener", () => {
+//   it("should navigate to the url shortener page", () => {
+//     cy.visit("http://localhost:3000/");
+//     cy.get('a[href="/url-shortener"]').first().click();
+//     cy.url().should("include", "/url-shortener");
+
+//     cy.get("h1").contains("Url Shortener");
+//   });
+//   // it("should create a new short link", () => {
+//   //   cy.visit("http://localhost:3000/url-shortener");
+//   //   cy.get('input[name="url"]').type("https://example.com");
+//   //   cy.get('button[type="submit"]').click();
+//   //   cy.get("h1").contains("Url Shortener");
+//   // });
+// });
